e655ca2f34ea39c82b96d202c21bc9b514aab19c
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-02-18 14:32:13 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Karthikbscllb0/Fs.git /Fs
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+fi
+cd /Fs
+pip3 install -U -r requirements.txt
+echo "Starting"
+python3 bot.py
